fb46778f0430794b27ddf5da5ccec11c0703e193
Ezequiel <zapata.ed1989@gmail.com>
2022-09-16 02:45:22 -0300

    nav

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll',function(){
     const img = document.getElementById("brand-img")
     const txt = document.getElementById("brand-txt")
     const nav = document.getElementById("navbar")
-    const qwe = document.getElementById("nav-link")
+  
 
     img.classList.toggle("off", window.scrollY>550)
     txt.classList.toggle("on", window.scrollY>550)
