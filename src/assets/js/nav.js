67bbf46aaf142de67cca045cd4e2aa3e7e12b39a
Ezequiel <zapata.ed1989@gmail.com>
2022-09-16 01:53:41 -0300

    primer comit nav

---
+++ src/assets/js/nav.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll',function(){
+    const img = document.getElementById("brand-img")
+    const txt = document.getElementById("brand-txt")
+    const nav = document.getElementById("navbar")
+    const qwe = document.getElementById("nav-link")
+
+    img.classList.toggle("off", window.scrollY>550)
+    txt.classList.toggle("on", window.scrollY>550)
+    nav.classList.toggle("small", window.scrollY>550)
+
+   
+})
