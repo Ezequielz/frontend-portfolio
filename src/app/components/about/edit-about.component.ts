82a6fdde73f33c468efbd487742d4d90f34e146b
Ezequiel <zapata.ed1989@gmail.com>
2023-01-13 11:18:05 -0300

    edit about valid

--- src/app/components/about/edit-about.component.ts
+++ src/app/components/about/edit-about.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { About } from 'src/app/model/about';
 import { AboutService } from 'src/app/services/about.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
 
 
@@ -13,10 +14,12 @@ import Swal from 'sweetalert2';
 export class EditAboutComponent implements OnInit {
 
   about: About = null;
+  form:FormGroup;
 
   constructor(private aboutService: AboutService ,
     private activatedRouter: ActivatedRoute,
-    private router: Router  
+    private router: Router ,
+    private formBuilder: FormBuilder
     ) { }
 
 ngOnInit(): void {
@@ -25,42 +28,68 @@ ngOnInit(): void {
 
   this.aboutService.detail(id).subscribe( data => {
     this.about = data;
+    this.form = this.formBuilder.group({
+      info:[data.info,[Validators.required]],
+      title:[data.title,[Validators.required]]
+    })
   }, err =>{
-    alert("Error al modificar el proyecto");
+    Swal.fire({
+      title: 'error al modificar proyecto',
+      icon:'error',
+      showConfirmButton: false,
+    })
     this.router.navigate(['']);
   });
   
 }
 
-onUpdate(){
+get Info(){
+  return this.form.get('info')
+}
+get Title(){
+  return this.form.get('title')
+}
 
-  Swal.fire({
-    title: 'Editando About',
-    text: 'Espere...',
-    showConfirmButton: false,
-  })
+onUpdate(event: Event){
+  event.preventDefault()
 
-  const id = this.activatedRouter.snapshot.params['id'];
-     
-  this.aboutService.update( id, this.about ).subscribe( data => {
+  if(this.form.value.info && this.form.value.title){
 
     Swal.fire({
-      icon: 'success',
-      title: 'About Editado',
-      timer: 1500,
-      showConfirmButton: false
+      title: 'Editando About',
+      text: 'Espere...',
+      showConfirmButton: false,
     })
-    
-    this.router.navigate(['']);
-  },err =>{
-    
+  
+    const id = this.activatedRouter.snapshot.params['id'];
+       
+    this.aboutService.update( id, this.form.value ).subscribe( data => {
+  
+      Swal.fire({
+        icon: 'success',
+        title: 'About Editado',
+        timer: 1500,
+        showConfirmButton: false
+      })
+      
+      this.router.navigate(['']);
+    },err =>{
+      
+      Swal.fire({
+        icon: 'error',
+        title: 'Error al modificar about',
+        timer: 1500,
+        showConfirmButton: false
+      })
+      this.router.navigate(['']);
+    });
+  }else{
     Swal.fire({
-      icon: 'error',
-      title: 'Error al modificar about',
-      timer: 1500,
-      showConfirmButton: false
+      title: 'Debe completar todos los campos',
+      icon:'error',
+      timer:1500,
+      showConfirmButton: false,
     })
-    this.router.navigate(['']);
-  });
+  }
 }
 }
