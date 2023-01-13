90eb3a38282a51232c0edf097ad0fa04b5a26fc2
Ezequiel <zapata.ed1989@gmail.com>
2023-01-13 20:49:55 -0300

    validaciones proyectos

--- src/app/components/projects/new-project.component.ts
+++ src/app/components/projects/new-project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Project } from 'src/app/model/project';
 import { ImageService } from 'src/app/services/image.service';
@@ -18,54 +19,91 @@ export class NewProjectComponent implements OnInit {
   giturl: string = '';
   weburl: string = '';
   saveImage: boolean = false;
+  form:FormGroup;
 
   constructor( private projectsService: ProjectsService,
     private activatedRouter: ActivatedRoute,
      private router: Router,
-     public imgService: ImageService
-      ) { }
+     public imgService: ImageService,
+     private formBuilder: FormBuilder
+      ) { 
+
+        this.form = this.formBuilder.group({
+          title:['',[Validators.required]],
+          descripcion:['',[Validators.required]],
+          img:['',[Validators.required]],
+          giturl:['',[Validators.required]],
+          weburl:['',[Validators.required]],
+  
+        })
+      }
 
   ngOnInit(): void {
   }
 
-  onCreate(): void{
+  get Title(){
+    return this.form.get('title')
+  }
+  get Descripcion(){
+    return this.form.get('descripcion')
+  }
+  get Img(){
+    return this.form.get('img')
+  }
+  get Giturl(){
+    return this.form.get('giturl')
+  }
+  get Weburl(){
+    return this.form.get('weburl')
+  }
 
-    const project = new Project(this.title, this.descripcion, this.img, this.giturl, this.weburl);
+  onCreate(event: Event): void{
+    event.preventDefault()
+    // const project = new Project(this.title, this.descripcion, this.img, this.giturl, this.weburl);
+    if(this.form.value.title && this.form.value.descripcion && this.form.value.img && this.form.value.giturl && this.form.value.weburl){
 
-    Swal.fire({
-      title: 'Creando Proyecto',
-      text: 'Espere...',
-      showConfirmButton: false,
-    })
-
-    project.img = this.imgService.url
-
-    
-    this.projectsService.save( project ).subscribe( data =>{
-      
       Swal.fire({
-        title: 'Proyecto creado correctamente',
-        text: "Quieres agregar otro?",
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Agregar'
-      }).then((result) => {
-        this.imgService.cleanUrl();
-        if (!result.isConfirmed) {
-          this.router.navigate(['']);
-        }
-      })
-    }, err =>{
-      Swal.fire({
-   
-        icon: 'error',
-        title: 'error al agregar proyecto',
+        title: 'Creando Proyecto',
+        text: 'Espere...',
         showConfirmButton: false,
-        timer: 2500
       })
-      this.router.navigate(['']);
-    });
+  
+      this.form.value.img = this.imgService.url
+  
+      
+      this.projectsService.save( this.form.value ).subscribe( data =>{
+        
+        Swal.fire({
+          title: 'Proyecto creado correctamente',
+          text: "Quieres agregar otro?",
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Agregar'
+        }).then((result) => {
+          this.imgService.cleanUrl();
+          if (!result.isConfirmed) {
+            this.router.navigate(['']);
+          }
+        })
+      }, err =>{
+        Swal.fire({
+     
+          icon: 'error',
+          title: 'error al agregar proyecto',
+          showConfirmButton: false,
+          timer: 2500
+        })
+        this.router.navigate(['']);
+      });
+    }else{
+      Swal.fire({
+        title: 'Debe completar todos los campos',
+        icon:'error',
+        timer:1500,
+        showConfirmButton: false,
+      })
+    }
   }
 
   uploadImage($event: any){
