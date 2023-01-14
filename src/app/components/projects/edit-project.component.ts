010e2bc711b0d1f5f4e34429739dc88ca74739db
Ezequiel <zapata.ed1989@gmail.com>
2023-01-13 21:04:18 -0300

    edit proyecto valid

--- src/app/components/projects/edit-project.component.ts
+++ src/app/components/projects/edit-project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Project } from 'src/app/model/project';
 import { ImageService } from 'src/app/services/image.service';
@@ -14,11 +15,13 @@ export class EditProjectComponent implements OnInit {
   
   project : Project = null;
   saveImage: boolean = false;
+  form:FormGroup;
 
   constructor(private projectsService: ProjectsService,
      private activatedRoute: ActivatedRoute,
       private router: Router,
-      public imgService: ImageService
+      public imgService: ImageService,
+      private formBuilder: FormBuilder 
        ) { }
 
   ngOnInit(): void {
@@ -26,46 +29,86 @@ export class EditProjectComponent implements OnInit {
     const id = this.activatedRoute.snapshot.params['id'];
     this.projectsService.detail(id).subscribe( data => {
       this.project = data;
+      this.form = this.formBuilder.group({
+        title:[data.title,[Validators.required]],
+        descripcion:[data.descripcion,[Validators.required]],
+        img:[data.img,[Validators.required]],
+        giturl:[data.giturl,[Validators.required]],
+        weburl:[data.weburl,[Validators.required]],
+
+      })
   
     }, err =>{
-      alert("Error al modificar el proyecto");
+      Swal.fire({
+        title: 'error al modificar proyecto',
+        icon:'error',
+        showConfirmButton: false,
+      })
       this.router.navigate(['']);
     });
     
   }
 
-  onUpdate(): void{
+  get Title(){
+    return this.form.get('title')
+  }
+  get Descripcion(){
+    return this.form.get('descripcion')
+  }
+  get Img(){
+    return this.form.get('img')
+  }
+  get Giturl(){
+    return this.form.get('giturl')
+  }
+  get Weburl(){
+    return this.form.get('weburl')
+  }
 
-    Swal.fire({
-      title: 'Editando Proyecto',
-      text: 'Espere...',
-      showConfirmButton: false,
-    })
 
-    const id = this.activatedRoute.snapshot.params['id'];
-    
-    if (this.imgService.url){
+  onUpdate(event: Event): void{
+    event.preventDefault()
 
-      this.project.img = this.imgService.url
+    if(this.form.value.title && this.form.value.descripcion && this.form.value.img && this.form.value.giturl && this.form.value.weburl){
+
+      Swal.fire({
+        title: 'Editando Proyecto',
+        text: 'Espere...',
+        showConfirmButton: false,
+      })
+  
+      const id = this.activatedRoute.snapshot.params['id'];
+      
+      if (this.imgService.url){
+  
+        this.form.value.img = this.imgService.url
+      }
+  
+      this.projectsService.update( id, this.form.value ).subscribe( data => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Proyecto Editado',
+          timer: 1500,
+          showConfirmButton: false
+        })
+        this.router.navigate(['']);
+      },err =>{
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al modificar proyecto',
+          timer: 1500,
+          showConfirmButton: false
+        })
+        this.router.navigate(['']);
+      });
+    }else{
+      Swal.fire({
+        title: 'Debe completar todos los campos',
+        icon:'error',
+        timer:1500,
+        showConfirmButton: false,
+      })
     }
-
-    this.projectsService.update( id, this.project ).subscribe( data => {
-      Swal.fire({
-        icon: 'success',
-        title: 'Proyecto Editado',
-        timer: 1500,
-        showConfirmButton: false
-      })
-      this.router.navigate(['']);
-    },err =>{
-      Swal.fire({
-        icon: 'error',
-        title: 'Error al modificar proyecto',
-        timer: 1500,
-        showConfirmButton: false
-      })
-      this.router.navigate(['']);
-    });
   }
 
   uploadImage($event: any){
