b489ad1b4c366b8349b2b0be3a1f0ebf916aa08c
Ezequiel <zapata.ed1989@gmail.com>
2023-01-13 21:21:15 -0300

    edit contact validaciones

--- src/app/components/contact/edit-contact.component.ts
+++ src/app/components/contact/edit-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Contact } from 'src/app/model/contact';
 import { ContactService } from 'src/app/services/contact.service';
@@ -14,10 +15,12 @@ export class EditContactComponent implements OnInit {
 
   contact: Contact = null;
   savePDF: boolean = false;
+  form:FormGroup;
 
   constructor(private contactService: ContactService,
      private router: Router,
-     public imgService: ImageService
+     public imgService: ImageService,
+     private formBuilder: FormBuilder 
       ) { }
 
 
@@ -27,44 +30,87 @@ export class EditContactComponent implements OnInit {
 
     this.contactService.detail(1).subscribe( data => {
       this.contact = data;
+      this.form = this.formBuilder.group({
+        city:[data.city,[Validators.required]],
+        cv:[data.cv,[Validators.required]],
+        email:[data.email,[Validators.required, Validators.email]],
+        github:[data.github,[Validators.required]],
+        linkedin:[data.linkedin,[Validators.required]],
+        tel:[data.tel,[Validators.required]]
+
+      })
   
     }, err =>{
-      alert("Error al modificar el contacto");
+      Swal.fire({
+        title: 'error al modificar contacto',
+        icon:'error',
+        showConfirmButton: false,
+      })
       this.router.navigate(['']);
     });
 
   }
 
-  onUpdate(){
+  get City(){
+    return this.form.get('city')
+  }
+  get Cv(){
+    return this.form.get('cv')
+  }
+  get Email(){
+    return this.form.get('email')
+  }
+  get Github(){
+    return this.form.get('github')
+  }
+  get Linkedin(){
+    return this.form.get('linkedin')
+  }
+  get Tel(){
+    return this.form.get('tel')
+  }
 
-    Swal.fire({
-      title: 'Editando Contacto',
-      text: 'Espere...',
-      showConfirmButton: false,
-    })
+  onUpdate(event: Event){
+    event.preventDefault()
 
-    if(this.imgService.url){
+    if(this.form.value.city && this.form.value.cv && this.form.value.email && this.form.value.github && this.form.value.linkedin && this.form.value.tel){
 
-      this.contact.cv = this.imgService.url
+      Swal.fire({
+        title: 'Editando Contacto',
+        text: 'Espere...',
+        showConfirmButton: false,
+      })
+  
+      if(this.imgService.url){
+  
+        this.form.value.cv = this.imgService.url
+      }
+  
+      this.contactService.update( 1,  this.form.value ).subscribe( data => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Contacto Editado',
+          timer: 1500,
+          showConfirmButton: false
+        })
+        this.router.navigate(['']);
+      },err =>{
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al modificar contacto',
+          timer: 1500,
+          showConfirmButton: false
+        })
+        this.router.navigate(['']);
+      });
+    }else{
+      Swal.fire({
+        title: 'Debe completar todos los campos',
+        icon:'error',
+        timer:1500,
+        showConfirmButton: false,
+      })
     }
-
-    this.contactService.update( 1, this.contact ).subscribe( data => {
-      Swal.fire({
-        icon: 'success',
-        title: 'Contacto Editado',
-        timer: 1500,
-        showConfirmButton: false
-      })
-      this.router.navigate(['']);
-    },err =>{
-      Swal.fire({
-        icon: 'error',
-        title: 'Error al modificar contacto',
-        timer: 1500,
-        showConfirmButton: false
-      })
-      this.router.navigate(['']);
-    });
     
   }
 
